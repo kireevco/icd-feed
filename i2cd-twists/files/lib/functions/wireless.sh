de009bb70bef9a33bf5b5b50e2f88e5e6c25b426
Henry Chang <mr.changyuheng@gmail.com>
2016-10-20 15:24:41 -0700

    Betterize fw_printenv usage

--- i2cd-twists/files/lib/functions/wireless.sh
+++ i2cd-twists/files/lib/functions/wireless.sh
@@ -1,13 +1,13 @@
 wireless_encrypt_passwd() {
 	local rawpass="$1"
-	local key=$(fw_printenv ethaddr | sed 's/.*=//g' | tr '[:upper:]' '[:lower:]')
+	local key=$(fw_printenv -n ethaddr | tr '[:upper:]' '[:lower:]')
 	local encpass=$(echo -n "$rawpass" | openssl aes-256-cbc -k "$key" | base64)
 	echo "$encpass"
 }
 
 wireless_decrypt_passwd() {
 	local encpass="$1"
-	local key=$(fw_printenv ethaddr | sed 's/.*=//g' | tr '[:upper:]' '[:lower:]')
+	local key=$(fw_printenv -n ethaddr | tr '[:upper:]' '[:lower:]')
 	local rawpass=$(echo -n "$encpass" | base64 -d | openssl aes-256-cbc -d -k "$key")
 	echo "$rawpass"
 }
