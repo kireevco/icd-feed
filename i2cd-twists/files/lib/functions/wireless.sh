9d987835f99a3f43318faf6dca8d3e7dc625d195
Henry Chang <mr.changyuheng@gmail.com>
2016-11-14 19:10:49 -0800

    Salt wmac to prevent from Wi-Fi confliction

--- i2cd-twists/files/lib/functions/wireless.sh
+++ i2cd-twists/files/lib/functions/wireless.sh
@@ -2,12 +2,12 @@ wireless_encrypt_passwd() {
 	local rawpass="$1"
 	local key=$(fw_printenv -n ethaddr | tr '[:upper:]' '[:lower:]')
 	local encpass=$(echo -n "$rawpass" | openssl aes-256-cbc -k "$key" | base64)
-	echo "$encpass"
+	echo -n "$encpass"
 }
 
 wireless_decrypt_passwd() {
 	local encpass="$1"
 	local key=$(fw_printenv -n ethaddr | tr '[:upper:]' '[:lower:]')
 	local rawpass=$(echo -n "$encpass" | base64 -d | openssl aes-256-cbc -d -k "$key")
-	echo "$rawpass"
+	echo -n "$rawpass"
 }
